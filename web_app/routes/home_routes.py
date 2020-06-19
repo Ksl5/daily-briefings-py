dd4abcf61d830d1d0d1f9c6ca4ffd6111f9c4eef
Ksl5 <65416583+Kiranlutfeali@users.noreply.github.com>
2020-06-19 01:54:45 -0400

    changes name

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -5,17 +5,17 @@ from flask import Blueprint, render_template, redirect, request, flash
 
 home_routes = Blueprint("home_routes", __name__)
 
-#@home_routes.route("/")
-#def index():
-#    print("VISITED THE HOME PAGE")
-#    #return render_template("dashboard.html")
-#    return "Welcome Home (TODO)"
-#
-#@home_routes.route("/about")
-#def about():
-#    print("VISITED THE ABOUT PAGE")
-#    return "About Me (TODO)"
-#
+@home_routes.route("/")
+def index():
+    print("VISITED THE HOME PAGE")
+    #return render_template("dashboard.html")
+    return "Welcome Home (TODO)"
+
+@home_routes.route("/about")
+def about():
+    print("VISITED THE ABOUT PAGE")
+    return "About Me (TODO)"
+
 @home_routes.route("/users/new")
 def register():
     print("VISITED THE REGISTRATION PAGE")
